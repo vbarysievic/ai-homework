4d58306632f05868ecbf4f41b04fd95d9d64f27c
vbarysievic <viktar.barysevich@innowise.com>
2025-06-06 09:02:30 +0200

    feat: task3

---
+++ task3/src/Dahboard.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export default function Dashboard() {
+  const [d, setD] = useState<number | null>(null);
+  useEffect(() => {
+    let t = 0;
+    for (let i = 0; i < 1e8; i++) {
+      t += i;
+    }
+    setD(t);
+  }, []);
+  return <div>{d}</div>;
+}
